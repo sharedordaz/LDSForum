5289f462ff6fe3806baf3aae7e782ca3f11d51b6
Shared Ordaz <sharedordaz@live.com.mx>
2023-03-10 02:35:22 -0600

    added good practices and git ignore

--- db/connect.js
+++ db/connect.js
@@ -3,24 +3,25 @@ dotenv.config();
 const MongoClient = require('mongodb').MongoClient;
 
 let _db;
-function startDB( callback ){
-  if (_db){
+function startDB(callback) {
+  if (_db) {
     console.log("Db is already started");
     return callback(null, _db);
   }
   MongoClient.connect(process.env.DB_URI)
-  .then((client) => {
-    _db = client;
-    callback(null, _db);
-    console.log("DB Connected")})
-  .catch((error) => {console.log("You got an error loser! " + error);})
+    .then((client) => {
+      _db = client;
+      callback(null, _db);
+      console.log("DB Connected")
+    })
+    .catch((error) => { console.log("You got an error loser! " + error); })
 };
 
-function getDB (){
- if (!_db){
-  throw Error("DB not initialized");
- }
- return _db;
+function getDB() {
+  if (!_db) {
+    throw Error("DB not initialized");
+  }
+  return _db;
 };
 
 module.exports = {
