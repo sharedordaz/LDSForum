c9a1943caa48295056cece9fdf8219ce6b782632
Shared Ordaz <sharedordaz@live.com.mx>
2023-03-10 23:47:52 -0600

    removed nodemon from start

--- controllers/users.js
+++ controllers/users.js
@@ -1,40 +1,90 @@
-const mongoclient = require("../db/connect.js"); 
+const mongoclient = require("../db/connect.js");
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res, next) => {
-  try{
+  try {
     dbo = mongoclient.getDB().db("LDSForum");
-    collection =  dbo.collection('main');
+    collection = dbo.collection('main');
     result = await collection.find();
-    resultArray = result.toArray().then( (lists) =>{
+    resultArray = result.toArray().then((lists) => {
       res.setHeader('Content-Type', 'application/json');
       res.status(200).json(lists);
     });
-    }
-  catch (e){
-      console.log("Funny error" + e )
+  }
+  catch (e) {
+    console.log("Funny error" + e)
   }
 }
 
-const createNew = async (req, res) =>{
-  
-  const doc = { firstName : req.body.firstName, 
-                lastName : req.body.lastName
-              };
+const getSingle = async (req, res, next) => {
+  try {
+    //get GET userId (from URL) req.params = $_GET in PHP
+    const userId = new ObjectId(req.params.id);
+    result = mongoclient.getDB().db("LDSForum").collection('main').find({ _id: userId })
+    await result.toArray().then((lists) => {
+      console.log('Single Contact Data example: \nId: ' + lists[0]);
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(lists[0]);
+    })
+  } catch (error) {
+    res.status(404).send("<p>Couldn't access to database!\n <hr>Error: </p>\t" + error);
+  }
+}
+
+const createNew = async (req, res) => {
+
+  const doc = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName
+  };
 
   const collection = mongoclient.getDB().db("LDSForum").collection("main");
 
-    
+
   const result = await collection.insertOne(doc);
-  if (result.acknowledged){
+  if (result.acknowledged) {
     res.status(201).json(result);
   } else {
-   res.status(500).json(result.error || 'Some error occurred while creating the contact.');
+    res.status(500).json(result.error || 'Some error occurred while creating the contact.');
   }
   console.log(`A document was inserted with the _id ${result.insertedId}`)
 }
 
+const updateUsr = async (req, res, next) => {
+  //res.send("PUT from function is working");
+  const userId = new ObjectId(req.params.id);
+  // be aware of updateOne if you only want to update specific fields
+  const doc = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+  };
+
+  const collection = mongoclient.getDB().db("LDSForum").collection("main");
+  const result = await collection.replaceOne({ _id: userId }, doc);
+  if (result.modifiedCount > 0) {
+    res.status(204).send("PUT request successful");
+  } else {
+    res.status(500).json(result.error || 'Some error occurred while updating the contact.');
+  }
+}
+const delUsr = async (req, res, next) => {
+  const userId = new ObjectId(req.params.id);
+
+  const collection = mongoclient.getDB().db("LDSForum").collection("main");
+  const result = await collection.deleteOne({ _id: userId }, true);
+
+  if (result.deletedCount > 0) {
+    res.status(204).send("DELETE request sucessful");
+  } else {
+    res.status(500).json(result.error || 'Some error occurred while deleting the contact.');
+  }
+};
+
+
 module.exports = {
   getAll,
-  createNew
+  getSingle,
+  createNew,
+  updateUsr,
+  delUsr
 };
